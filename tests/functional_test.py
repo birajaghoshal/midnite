b3250964c9031413916dfa5a8457a1a06ac8f1a5
Fabian Huch <fabian@luminovo.ai>
2019-03-11 14:46:48 +0100

    More tests

--- tests/functional_test.py
+++ tests/functional_test.py
@@ -27,10 +27,16 @@ def test_mutual_information():
     """Test the mutual information function.
 
     """
-    input_ = torch.tensor([[0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0]])
+    input_ = torch.tensor([[1.0, 0.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0]])
     output = F.mutual_information(input_)
 
-    print(output)
+    assert output.allclose(torch.tensor([0.5004]))
+
+    # More confident prediction
+    input_ = torch.tensor([[0.2, 0.8], [0.3, 0.7], [0.1, 0.9]])
+    output = F.mutual_information(input_)
+
+    assert output.allclose(torch.tensor([0.02162]), rtol=1e-4)
 
 
 def test_variation_ratio():
