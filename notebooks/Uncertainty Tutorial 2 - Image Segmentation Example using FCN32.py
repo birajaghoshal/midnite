4c8fdf30c25e8739bcdde7428f3aa36e06914d09
Fabian Huch <fabian@luminovo.ai>
2019-03-18 12:26:11 +0100

    Re-added notebooks comment that jupyter ate

--- notebooks/Uncertainty Tutorial 2 - Image Segmentation Example using FCN32.py
+++ notebooks/Uncertainty Tutorial 2 - Image Segmentation Example using FCN32.py
@@ -21,8 +21,14 @@ from interpretability_framework import modules
 
 
 # # Interpretability-framework - Uncertainty Tutorial 2
+# in this notebook you will learn how to use the interpretability framework in an image segmentation example and how to interpret the results.
+# 
+# ## Segmentation example with FCN32s
+# Demonstration of uncertainty measurement and visualization in an segmentation example.
+# 
 # 
 # ### Step 1: Load pre-trained FCN32 model
+# **Note: Monte Carlo Dropout ensembles only achieve proper results when used on a net which was trained with dropout. So check if the model you would like to use has dropout layers active in training.**
 
 # In[2]:
 
@@ -77,3 +83,13 @@ print("Mutual information (model uncertainty):")
 plt.imshow(mutual_info.sum(dim=1).squeeze(dim=0).detach().numpy())
 plt.show()
 
+
+# ## Interpretation of results 
+# In contrast to the image classification example, we calculate predictions and uncertainties for each pixel of the image. 
+# Hence, we can visualize the pixel uncertainties as an image and add a color scheme to make interpretations easier (red = high uncertainty, blue = low uncertainty).
+# 
+# What can we see in these uncertainty images?
+# 
+# * **Total predictive entropy:** the total predictive uncertainty of each pixel, we can detect which pixels have high uncertainty and therefore, are predicted with low confidence.
+# * **Mutual Information:** the model uncertainty of each pixel, we can detect which pixels are predicted with low confidence due to the model.
+# * **Data Uncertainty:** we can detect which pixels are predicted with low confidence due to the quality of the data (image).
