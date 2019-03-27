d56e3da70edfc4274062f6c31e1b206083cd86ee
Christina <christina-aigner>
2019-03-27 18:36:50 +0100

    added interface tests for get_value method

--- tests/visualization/building_blocks_test.py
+++ tests/visualization/building_blocks_test.py
@@ -1,4 +1,5 @@
 """Test for the building blocks"""
+import torch
 from assertpy import assert_that
 from numpy.testing import assert_array_equal
 
@@ -65,3 +66,30 @@ def test_neuron_selector():
     selection = NeuronSelector(SpatialSplit(), [0, 1]).get_mask((2, 2, 2)).numpy()
 
     assert_array_equal(selection, [[[0, 1], [0, 0]], [[0, 1], [0, 0]]])
+
+
+def test_neuron_value():
+    """Test if the neuron selector correctly retrieves a neuron value from an input tensor"""
+    # use test input with unique values and dimensions
+    input_3d = torch.tensor([[[1, 2, 3, 4, 5]], [[6, 7, 8, 9, 10]]])
+    input_1d = torch.tensor([1, 2, 3, 4, 5])
+    value = NeuronSelector(NeuronSplit(), [0, 0, 0]).get_value(input_3d)
+    assert_array_equal(value.numpy(), 1)
+    value = NeuronSelector(NeuronSplit(), [2]).get_value(input_1d)
+    assert_array_equal(value.numpy(), 3)
+
+
+def test_channel_value():
+    """Test if the neuron selector correctly retrieves a channel from an input tensor"""
+    # use test input with unique values and dimensions
+    input_ = torch.tensor([[[1, 2, 3, 4, 5]], [[6, 7, 8, 9, 10]]])
+    value = NeuronSelector(ChannelSplit(), [0]).get_value(input_)
+    assert_array_equal(value, [[1, 2, 3, 4, 5]])
+
+
+def test_spatial_value():
+    """Test if the neuron selector correctly retrieves a spatial from an input tensor"""
+    # use test input with unique values and dimensions
+    input_ = torch.tensor([[[1, 2, 3, 4, 5]], [[6, 7, 8, 9, 10]]])
+    value = NeuronSelector(SpatialSplit(), [0, 0]).get_value(input_)
+    assert_array_equal(value, [1, 2, 3, 4, 5])
